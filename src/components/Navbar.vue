c6505695e6b90beb71c10b9282d22960bdf35d7c
sshanto13 <sumancsediu@gmail.com>
2021-04-05 09:37:10 +0600

    update search

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,6 @@
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-
             <form @submit.prevent="search" class="d-flex">
               <input v-model="keyword" class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-success" type="submit">Search</button>
